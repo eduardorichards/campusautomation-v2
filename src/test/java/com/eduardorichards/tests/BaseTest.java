e3c527a37d8da31e5f954faf419f36fa17259c75
Eduardo <cutisunr@gmail.com>
2026-08-14 18:04:05 -0300

    add screenshot on test failure

--- src/test/java/com/eduardorichards/tests/BaseTest.java
+++ src/test/java/com/eduardorichards/tests/BaseTest.java
@@ -2,14 +2,17 @@ package com.eduardorichards.tests;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 
 import com.eduardorichards.core.driver.DriverManager;
+import com.eduardorichards.core.listeners.ScreenshotListener;
 
 /**
  * Base class for all test classes.
  * Handles WebDriver lifecycle (creation and teardown) so that each test
  * method runs with a fresh, isolated browser session on its own thread.
  */
+@Listeners(ScreenshotListener.class)
 public class BaseTest {
 
     @BeforeMethod
