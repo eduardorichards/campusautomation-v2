89ce66130ab62be3f9c5810e80cb3291bc119f7b
Eduardo <cutisunr@gmail.com>
2026-08-17 13:53:16 -0300

    add headless mode option for chrome and firefox

--- src/main/java/com/eduardorichards/core/driver/DriverManager.java
+++ src/main/java/com/eduardorichards/core/driver/DriverManager.java
@@ -6,8 +6,9 @@ import java.util.function.Supplier;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,9 +20,24 @@ public class DriverManager {
     private static final Logger log = LoggerFactory.getLogger(DriverManager.class);
 
     private static final Map<String, Supplier<WebDriver>> BROWSER_FACTORIES = Map.of(
-            "chrome", ChromeDriver::new,
-            "firefox", FirefoxDriver::new,
-            "edge", EdgeDriver::new);
+            "chrome", DriverManager::createChromeDriver,
+            "firefox", DriverManager::createFirefoxDriver);
+
+    private static WebDriver createChromeDriver() {
+        ChromeOptions options = new ChromeOptions();
+        if (ConfigReader.isHeadless()) {
+            options.addArguments("--headless=new", "--window-size=1920,1080");
+        }
+        return new ChromeDriver(options);
+    }
+
+    private static WebDriver createFirefoxDriver() {
+        FirefoxOptions options = new FirefoxOptions();
+        if (ConfigReader.isHeadless()) {
+            options.addArguments("-headless", "--width=1920", "--height=1080");
+        }
+        return new FirefoxDriver(options);
+    }
 
     private static WebDriver createDriver() {
         String browser = ConfigReader.getBrowser().toLowerCase();
